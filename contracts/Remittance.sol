b77d475fa073c1577261a0e51baa3881d6729676
Wojciech Kalinski <wojciech.kalinski@espeo.eu>
2018-10-02 12:29:32 +0200

    Remake of solving puzzle process(according to B9 Lab academy tip I have used keccak256 to encrypt)

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -2,14 +2,12 @@ pragma solidity ^0.4.24;
 
 
 contract Remittance {
-    uint ownedWeis;
-    address owner;
 
-    WalletOwner[2] owners;
+    address public owner;
 
     uint deadline = block.number + 1000;
 
-    modifier validEtherSend {
+    modifier onlyIfValidEther {
         require(msg.value > 0);
         _;
     }
@@ -24,60 +22,57 @@ contract Remittance {
         _;
     }
 
-    modifier onlyIfBob {
-        require(msg.sender == owners[1].owner);
-        _;
-    }
-
     function kill() onlyIfOwner private {
         selfdestruct(owner);
     }
 
-    mapping(address => uint) authentications;
-
     event LogEtherSended(uint _amount);
     event LogEtherConverted(uint _amount);
+    event LogRemittanceCreated(address _recipient);
+    event LogPuzzleSolved(RemittanceStruct _remittance);
+    event LogEtherWithdrawal(uint _amount);
 
-    struct WalletOwner {
+    struct RemittanceStruct {
         uint balance;
         address owner;
+        bytes32 puzzle;
     }
 
-    constructor(address _bob, address _carol) public payable {
+    constructor() public payable {
         owner = msg.sender;
-        owners[0].balance = 0;
-        owners[1].balance = 0;
-        owners[0].owner = _bob;
-        owners[1].owner = _carol;
     }
+    mapping(address => RemittanceStruct) public remittances;
 
-    function authenticate(uint authenticationToken) private {
-        require(authenticationToken > 0);
-        address prover = msg.sender;
-        authentications[prover] = authenticationToken;
-    }
-
-    function getAuthenticationToken(address prover) private constant returns (uint) {
-        return authentications[prover];
-    }
-
-    function transferEther(uint _amount) public validEtherSend returns (bool success) {
-        require(_amount > 0);
-        owners[1].balance += _amount;
-        uint authenticator = getAuthenticationToken(msg.sender);
-        authenticate(authenticator);
-        emit LogEtherSended(_amount);
+    function solvePuzzle(string _puzzle) public returns (bool success) {
+        RemittanceStruct storage remittance = remittances[msg.sender];
+        require(remittance.amount > 0);
+        require(encrypt(_puzzle, msg.sender) == remittance.puzzle);
+        uint amount = remittance.amount;
+        delete remittances[msg.sender];
+        emit LogPuzzleSolve(remittance);
+        emit LogWithdrawal(remittance.owner, msg.sender, remittance.amount);
+        msg.sender.transfer(amount);
         return true;
     }
 
-    function convertEther(uint _amount) private validEtherSend view returns (uint convert) {
-        uint converted = _amount * 2;
-        return converted;
-    }
+    function createRemittance(address _recipient, bytes32 _password) public payable returns (bool success) {
+        require(owner > 0);
+        require(_recipient > address(0x0));
+        require(_password != bytes32(0));
 
-    function claimRefund() public onlyIfDeadline returns (bool success) {
-        uint amount = msg.sender.balance;
-        owners[1].balance -= amount;
+        RemittanceStruct memory remittance;
+        remittance.owner = msg.sender;
+        remittance.balance = msg.value;
+        remittance.puzzle = _password;
+
+        remittances[_recipient] = remittance;
         return true;
     }
+
+
+    function encrypt(string _password, address _address) public pure returns (bytes32) {
+        bytes32 result = keccak256(abi.encodePacked(_password, _address));
+        return result;
+    }
+
 }
